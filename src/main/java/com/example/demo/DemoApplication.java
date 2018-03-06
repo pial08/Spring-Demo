8115609cafd917e037d6b1d1fd923e9fa06a6016
pial08 <pialcsedu19@gmail.com>
2018-03-06 23:13:03 +0600

    First Commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.pial.springbootstarter")
+//@ComponentScan(basePackages = "com.pial.springbootstarter.topic")
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+
+}
